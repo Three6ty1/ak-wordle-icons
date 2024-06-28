31518eb88aa60d08b412dc9c2f2537c13aec7ddb
Three6ty1 <wk.zhang02@gmail.com>
2024-06-28 20:51:04 +1000

    Added comment on where to install exe file

--- avatars/convert.sh
+++ avatars/convert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Need to download cwebp and put /bin/.exe into this directory https://developers.google.com/speed/webp/download 
+
 PARAMS=('-m 6 -q 70 -mt -af -progress')
 
 if [ $# -ne 0 ]; then
