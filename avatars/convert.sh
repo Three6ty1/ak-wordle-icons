d9bb3351781946515791b3bbc961de3062003e5f
Three6ty1 <wk.zhang02@gmail.com>
2024-06-28 21:10:01 +1000

    Added friston-3 icons

--- avatars/convert.sh
+++ avatars/convert.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Need to download cwebp and put /bin/.exe into this directory https://developers.google.com/speed/webp/download 
+# Need to run in the same directory as avatars
 
-PARAMS=('-m 6 -q 70 -mt -af -progress')
+PARAMS=('-m 6 -q 70 -mt -af -progress -resize 180 180')
 
 if [ $# -ne 0 ]; then
 	PARAMS=$@;
